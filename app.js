f89b9e6f129acb357a9e6cb2d15853f2bc781dde
Tammy Castro <tammycastro@gmail.com>
2017-06-20 23:15:54 -0400

    update of app.js, .mustache files

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const data = require('./data.js');
+const mustacheExpress = require('mustache-express');
 
-var application = express();
+const application = express();
 
 application.engine('mustache', mustacheExpress());
+application.set('views', './views');
+application.set('view engine', 'mustache');
 
-application.get('/', (request, response) => {
-    response.send('Hello World!');
+application.use('/public', express.static('./public'));
+application.get('/', function(request, response){
+    response.render('index', data);
 });
 
-application.listen(3000, () => {
-    
+application.get('/:id', (request, response) => {
+    let id = parseInt(required.params.id);
+    let user = data.users.filter(function(obj, index){
+        if(obj.id === id){
+            return true;
+        }
+        else{
+            return false;
+        }
+    })
+    response.render('user', user[0]);
 });
+
+
+application.listen(3000, 'localhost');
