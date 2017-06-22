823e94d8e85a3e9a7246a83829b3b46d9c9686e8
Tammy Castro <tammycastro@gmail.com>
2017-06-22 18:30:27 -0400

    update of html and css files

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
 const express = require('express');
 const data = require('./data.js');
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
 
 const application = express();
 
 application.engine('mustache', mustacheExpress());
 application.set('views', './views');
 application.set('view engine', 'mustache');
+    
+application.use(bodyParser.urlencoded());
 
-application.use('/public', express.static('./public'));
+application.use(express.static('public'));
+
 application.get('/', function(request, response){
     response.render('index', data);
 });
 
-application.get('/:id', (request, response) => {
-    let id = parseInt(required.params.id);
-    let user = data.users.filter(function(obj, index){
-        if(obj.id === id){
-            return true;
-        }
-        else{
-            return false;
-        }
-    })
-    response.render('user', user[0]);
+application.get('/:id', function(request, response){
+    var id = parseInt(request.params.id);
+    var profile = data.users.filter(
+        function(obj, index){
+            if (obj.id === id) {
+                return true;
+            }
+            else{
+                return false;
+            }
+    });
+    response.render('profile', profile[0]);
 });
-
 
 application.listen(3000, 'localhost');
